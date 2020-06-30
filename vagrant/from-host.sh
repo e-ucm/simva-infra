3f79c98949c324a33123ede9bb99cb90797c45d5
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-06-30 22:49:48 +0200

    Add kafka stack to sync

--- vagrant/from-host.sh
+++ vagrant/from-host.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
-dirs="00-network 01-traefik 02-keycloak 03-limesurvey 04-minio"
+dirs="docker-stacks 00-network 01-traefik 02-keycloak 03-limesurvey 04-minio 05-kafka"
 for d in $dirs; do
-  rsync -avh --itemize-changes --delete "/vagrant/$d/" "/home/vagrant/$d/"
+  rsync -avh --itemize-changes --delete --exclude "data" "/vagrant/$d/" "/home/vagrant/$d/"
 done
