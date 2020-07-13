b496a33e6670b289b04b6cf67dff467ac69a1285
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-07-13 22:58:48 +0200

    Adding kafka management hooks

---
+++ 05-kafka/etc/hooks/after-start.d/10-configure-sink.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -euo pipefail
+[[ "${DEBUG:-false}" == "true" ]] && set -x
+
+wait_time=10;
+count=20;
+done="ko";
+while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
+    echo 1>&2 "Checking minio: $((20-$count+1)) pass";
+    set +e
+    docker-compose exec connect curl -f -sS http://connect.${SIMVA_INTERNAL_DOMAIN}:8083/
+    ret=$?;
+    set -e
+    if [ $ret -eq 0 ]; then
+    done="ok";
+    else
+    echo 1>&2 "Kafka connect not available, waiting ${wait_time}s";
+    sleep ${wait_time};
+    fi;
+    count=$((count-1));
+done;
+if [ $count -eq 0 ]; then
+    echo 1>&2 "Kafka connect not available !";
+    exit 1
+fi;
+
+connector_name=$(jq '.name' "${SIMVA_CONFIG_HOME}/kafka/connect/simva-sink-template.json" -r)
+
+set +e
+stack exec connect curl -f -sS \
+  --header 'Content-Type: application/json' \
+  --header 'Accept: application/json' \
+  http://connect.${SIMVA_INTERNAL_DOMAIN}:8083/connectors/${connector_name} >/dev/null 2>&1
+ret=$?
+set -e
+
+if [[ $ret -ne 0 ]]; then
+
+  jq_script=$(cat <<'JQ_SCRIPT'
+  .config["store.url"]=$minioUrl
+    | .config["aws.access.key.id"]=$minioUser
+      | .config["aws.secret.access.key"]=$minioSecret
+        | .
+JQ_SCRIPT
+)
+  cat ${SIMVA_CONFIG_HOME}/kafka/connect/simva-sink-template.json | jq \
+  --arg minioUrl "${SIMVA_KAFKA_CONNECT_SINK_MINIO_URL}" \
+  --arg minioUser "${SIMVA_KAFKA_CONNECT_SINK_USER}" \
+  --arg minioSecret "${SIMVA_KAFKA_CONNECT_SINK_SECRET}" \
+  "$jq_script" > "${SIMVA_CONFIG_HOME}/kafka/connect/simva-sink.json"
+
+  connector_name=$(jq '.name' "${SIMVA_CONFIG_HOME}/kafka/connect/simva-sink.json" -r)
+
+  docker-compose exec connect curl -f -sS \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/json' \
+    --request POST \
+    --data '@/usr/share/simva/simva-sink.json' \
+    http://connect.${SIMVA_INTERNAL_DOMAIN}:8083/connectors >/dev/null 2>&1
+fi
