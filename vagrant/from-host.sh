ebc63005ca065984c596694003ab7e40e7b290dd
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-07-16 00:47:54 +0200

    Installation and launch are separated

--- vagrant/from-host.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[[ "${DEBUG:-false}" == "true" ]] && set -x
-
-dirs="docker-stacks 00-network 01-traefik 02-keycloak 03-limesurvey 04-minio 05-kafka"
-for d in $dirs; do
-  rsync -avh --itemize-changes --delete --exclude "data" "/vagrant/$d/" "/home/vagrant/$d/"
-done
