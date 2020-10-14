b638c37d08fb2443ab6687ae103296af96548e59
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-10-14 11:16:12 +0200

    Modify domains assigned to generated certificates

--- docker-stacks/01-traefik/etc/hooks/before-install.d/10-create-wildcard-certificate.sh
+++ docker-stacks/01-traefik/etc/hooks/before-install.d/10-create-wildcard-certificate.sh
@@ -20,10 +20,10 @@ if [[ ! -e "${SIMVA_TLS_HOME}/traefik.pem" ]]; then
         -cert-file ${SIMVA_TLS_HOME}/traefik.pem \
         -key-file ${SIMVA_TLS_HOME}/traefik-key.pem \
             "traefik.${SIMVA_INTERNAL_DOMAIN}" \
+            "${SIMVA_INTERNAL_DOMAIN}" \
+            "*.${SIMVA_INTERNAL_DOMAIN}" \
+            "${SIMVA_EXTERNAL_DOMAIN}" \
             "*.${SIMVA_EXTERNAL_DOMAIN}" \
-            "*.keycloak.${SIMVA_EXTERNAL_DOMAIN}" \
-            "*.limesurvey.${SIMVA_EXTERNAL_DOMAIN}" \
-            "*.app.${SIMVA_EXTERNAL_DOMAIN}" \
             "localhost" \
             "127.0.0.1" \
             "${SIMVA_HOST_EXTERNAL_IP}"
