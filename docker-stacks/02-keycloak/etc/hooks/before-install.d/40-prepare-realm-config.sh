840f90aae4d4b8d1c4540b5a0d36e88575f3a76a
Iván "Synpheros" Perez <synpheros@hotmail.com>
2024-01-22 16:33:14 +0100

    Disabled duplicated cert generation code on keycloak realm config

--- docker-stacks/02-keycloak/etc/hooks/before-install.d/40-prepare-realm-config.sh
+++ docker-stacks/02-keycloak/etc/hooks/before-install.d/40-prepare-realm-config.sh
@@ -31,21 +31,25 @@ __file_env() {
     fi
 }
 
-if [[ ! -e "${SIMVA_TLS_HOME}/limesurvey.pem" ]]; then
-    openssl genrsa -out ${SIMVA_TLS_HOME}/limesurvey-key.pem 2048
-    openssl req \
-        -subj "${SIMVA_LIMESURVEY_SIMPLESAMLPHP_SP_CERT_SUBJ}" \
-        -new -key ${SIMVA_TLS_HOME}/limesurvey-key.pem \
-        -out ${SIMVA_TLS_HOME}/limesurvey.csr
-
-    export CAROOT=${SIMVA_TLS_HOME}
-    mkcert \
-        -cert-file ${SIMVA_TLS_HOME}/limesurvey.pem \
-        -csr ${SIMVA_TLS_HOME}/limesurvey.csr
-
-    cp ${SIMVA_TLS_HOME}/limesurvey.pem ${SIMVA_TLS_HOME}/limesurvey-fullchain.pem
-    cat ${SIMVA_TLS_HOME}/rootCA.pem >> ${SIMVA_TLS_HOME}/limesurvey-fullchain.pem
-fi
+# #################################
+# ### DISABLED DUPLICATED CODE  ###
+# #################################
+# if [[ ! -e "${SIMVA_TLS_HOME}/limesurvey.pem" ]]; then
+#     openssl genrsa -out ${SIMVA_TLS_HOME}/limesurvey-key.pem 2048
+#     openssl req \
+#         -subj "${SIMVA_LIMESURVEY_SIMPLESAMLPHP_SP_CERT_SUBJ}" \
+#         -new -key ${SIMVA_TLS_HOME}/limesurvey-key.pem \
+#         -out ${SIMVA_TLS_HOME}/limesurvey.csr
+# 
+#     export CAROOT=${SIMVA_TLS_HOME}
+#     mkcert \
+#         -cert-file ${SIMVA_TLS_HOME}/limesurvey.pem \
+#         -csr ${SIMVA_TLS_HOME}/limesurvey.csr
+# 
+#     cp ${SIMVA_TLS_HOME}/limesurvey.pem ${SIMVA_TLS_HOME}/limesurvey-fullchain.pem
+#     cat ${SIMVA_TLS_HOME}/rootCA.pem >> ${SIMVA_TLS_HOME}/limesurvey-fullchain.pem
+# fi
+# #################################
 
 # Oneline certificate
 limesurvey_cert=$(cat "${SIMVA_TLS_HOME}/limesurvey.pem" | tail -n +2 | head -n -1 | sed ':a;N;$!ba;s/\n//g')
