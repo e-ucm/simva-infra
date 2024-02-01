30608d3fcfa6dc12f16c1fb69f3a9faf2be7fa6a
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-02-01 21:14:54 +0100

    fix config script to set iframe allow

--- docker-stacks/03-limesurvey/etc/hooks/before-install.d/01-add-config.sh
+++ docker-stacks/03-limesurvey/etc/hooks/before-install.d/01-add-config.sh
@@ -1,9 +1,10 @@
-file_input = $(cat ${SIMVA_CONFIG_HOME}/limesurvey/config.php)
-newstr=$(
-    echo "${file_input}  
-    | sed  's/<<SIMVA_LIMESURVEY_MYSQL_DATABASE>>/$(SIMVA_LIMESURVEY_MYSQL_DATABASE)/g'
-    | sed  's/<<SIMVA_INTERNAL_DOMAIN>>/$(SIMVA_INTERNAL_DOMAIN)/g'
-    | sed  's/<<SIMVA_LIMESURVEY_MYSQL_USER>>/$(SIMVA_LIMESURVEY_MYSQL_USER)/g'
-    | sed  's/<<SIMVA_LIMESURVEY_MYSQL_PASSWORD>>/$(SIMVA_LIMESURVEY_MYSQL_PASSWORD)/g'
-)
-echo newstr > ${SIMVA_DATA_HOME}/limesurvey/data/etc/config.php 
+#!/bin/bash
+
+config_contents=$(<"${SIMVA_CONFIG_HOME}/limesurvey/config.php")
+
+echo "${config_contents}" \
+    | sed  "s/<<SIMVA_LIMESURVEY_MYSQL_DATABASE>>/${SIMVA_LIMESURVEY_MYSQL_DATABASE}/g" \
+    | sed  "s/<<SIMVA_INTERNAL_DOMAIN>>/${SIMVA_INTERNAL_DOMAIN}/g" \
+    | sed  "s/<<SIMVA_LIMESURVEY_MYSQL_USER>>/${SIMVA_LIMESURVEY_MYSQL_USER}/g" \
+    | sed  "s/<<SIMVA_LIMESURVEY_MYSQL_PASSWORD>>/${SIMVA_LIMESURVEY_MYSQL_PASSWORD}/g" \
+    > "${SIMVA_DATA_HOME}/limesurvey/data/etc/config.php"
