4e4b2817c8c3bb66f7c84af162c7081543400e0f
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-02-05 09:49:05 +0100

    generate config files for 05 kafka stack

---
+++ docker-stacks/05-kafka/etc/hooks/before-install.d/20-generate-config-file-kafka-ui.sh
@@ -0,0 +1,11 @@
+# Creating Kafka Connect UI config
+echo ${SIMVA_DNS_SERVICE_IP} > "${SIMVA_CONFIG_HOME}/kafka/connect-ui/resolv.conf"
+echo "options edns0 ndots:0" >>"${SIMVA_CONFIG_HOME}/kafka/connect-ui/resolv.conf"
+
+# Creating Kafka Schema Registry UI config
+echo ${SIMVA_DNS_SERVICE_IP} > "${SIMVA_CONFIG_HOME}/kafka/schema-registry-ui/resolv.conf"
+echo "options edns0 ndots:0" >>"${SIMVA_CONFIG_HOME}/kafka/schema-registry-ui/resolv.conf"
+
+# Creating Kafka Topics UI config
+echo ${SIMVA_DNS_SERVICE_IP} > "${SIMVA_CONFIG_HOME}/kafka/topics-ui/resolv.conf"
+echo "options edns0 ndots:0" >>"${SIMVA_CONFIG_HOME}/kafka/topics-ui/resolv.conf"
