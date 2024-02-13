8cebb9260c5da94aa8fc1cfc3232a906d8e03a8c
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-02-13 15:46:36 +0100

    Update config.php for LimeSurvey stack

--- docker-stacks/03-limesurvey/etc/hooks/before-install.d/01-add-config.sh
+++ docker-stacks/03-limesurvey/etc/hooks/before-install.d/01-add-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-config_contents=$(<"${SIMVA_CONFIG_HOME}/limesurvey/config.php")
+config_contents=$(<"${SIMVA_CONFIG_HOME}/limesurvey/etc-template/config.php")
 
 echo "${config_contents}" \
     | sed  "s/<<SIMVA_LIMESURVEY_MYSQL_DATABASE>>/${SIMVA_LIMESURVEY_MYSQL_DATABASE}/g" \
     | sed  "s/<<SIMVA_INTERNAL_DOMAIN>>/${SIMVA_INTERNAL_DOMAIN}/g" \
     | sed  "s/<<SIMVA_LIMESURVEY_MYSQL_USER>>/${SIMVA_LIMESURVEY_MYSQL_USER}/g" \
     | sed  "s/<<SIMVA_LIMESURVEY_MYSQL_PASSWORD>>/${SIMVA_LIMESURVEY_MYSQL_PASSWORD}/g" \
-    > "${SIMVA_DATA_HOME}/limesurvey/data/etc/config.php"
+    > "${SIMVA_CONFIG_HOME}/limesurvey/etc/config.php"
