3252920bcdff44f169da6d0e5e4ac46147e35e77
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-02-19 10:36:22 +0100

    update purge.d to remove config policies at purge step

--- docker-stacks/04-minio/etc/hooks/purge.d/01-remove-data-and-config.sh
+++ docker-stacks/04-minio/etc/hooks/purge.d/01-remove-data-and-config.sh
@@ -4,3 +4,10 @@
 # Removing Minio data
 cd "${SIMVA_DATA_HOME}/minio"
 rm -rf ./*
+
+#####################
+# REMOVE MINIO CONFIG
+#####################
+# Removing Minio policies configuration
+cd "${SIMVA_CONFIG_HOME}/minio/policies"
+rm -rf ./*
