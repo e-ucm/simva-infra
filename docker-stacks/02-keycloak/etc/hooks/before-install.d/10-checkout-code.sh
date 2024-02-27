98fbaf705a23ffefbf84467b91a9dd5303bd4b39
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-02-27 13:05:07 +0100

    adding keycloak event jar 0.3

--- docker-stacks/02-keycloak/etc/hooks/before-install.d/10-checkout-code.sh
+++ docker-stacks/02-keycloak/etc/hooks/before-install.d/10-checkout-code.sh
@@ -6,9 +6,11 @@ SIMVA_KEYCLOAK_EVENT_GIT_REPO_URL=https://github.com/p2-inc/keycloak-events.git
 SIMVA_KEYCLOAK_EVENT_GIT_REF=${SIMVA_KEYCLOAK_EVENT_GIT_REF:-v0.3}
 
 # Create source folder
-mkdir -p ${STACK_HOME}/extensions/keycloak-events
+#mkdir -p ${STACK_HOME}/extensions/keycloak-events
 
 # Checkout code in temp dir
-tmp_dir=$(mktemp -d)
-git clone --depth 1 --branch ${SIMVA_KEYCLOAK_EVENT_GIT_REF} ${SIMVA_KEYCLOAK_EVENT_GIT_REPO_URL} ${tmp_dir} > /dev/null 2>&1;
-rsync -avh --delete --itemize-changes ${tmp_dir}/ ${STACK_HOME}/extensions/keycloak-events > /dev/null 2>&1
+#tmp_dir=$(mktemp -d)
+#git clone --depth 1 --branch ${SIMVA_KEYCLOAK_EVENT_GIT_REF} ${SIMVA_KEYCLOAK_EVENT_GIT_REPO_URL} ${tmp_dir#} > /dev/null 2>&1;
+#rsync -avh --delete --itemize-changes ${tmp_dir}/ ${STACK_HOME}/extensions/keycloak-events > /dev/null 2>&1
+
+cp "${STACK_HOME}/extensions/keycloak-events-0.3.jar" "${SIMVA_DATA_HOME}/keycloak/deployments"
