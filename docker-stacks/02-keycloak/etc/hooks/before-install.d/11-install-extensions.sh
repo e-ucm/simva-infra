7be4b835ed5fbf6808893fa0d126ce0632bd3835
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-03-07 12:09:11 +0100

    update checkout code of keycloak events + installing git in maven docker

--- docker-stacks/02-keycloak/etc/hooks/before-install.d/11-install-extensions.sh
+++ docker-stacks/02-keycloak/etc/hooks/before-install.d/11-install-extensions.sh
@@ -22,9 +22,9 @@ for extension in $(find ${EXTENSIONS_DIR} -mindepth 1 -maxdepth 1 -type d); do
             -v ${SIMVA_DATA_HOME}/maven/m2:/usr/src/mymaven/.m2 \
             -u $(id -u ${USER}):$(id -g ${USER}) \
             -e MAVEN_CONFIG=/usr/src/mymaven/.m2 \
-            maven:3.8.7-openjdk-18-slim mvn -Duser.home=/usr/src/mymaven clean package
+            maven:3.8.7-openjdk-18-slim sh -c "apt update && apt install -y git && mvn -Duser.home=/usr/src/mymaven clean package"
     fi
-    cp "${extension}/target${extension_name}.jar" "${SIMVA_DATA_HOME}/keycloak/deployments"
+    cp "${extension}/target${extension_name}*.jar" "${SIMVA_DATA_HOME}/keycloak/deployments"
 done
 chmod a+r ${SIMVA_DATA_HOME}/keycloak/deployments
 chmod a+w ${SIMVA_DATA_HOME}/keycloak/deployments
