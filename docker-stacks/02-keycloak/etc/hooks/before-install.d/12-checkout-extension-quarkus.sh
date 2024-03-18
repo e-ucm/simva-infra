474564874320a9ca8fde4bb2b766ed6564a274b0
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-03-18 15:12:27 +0100

    adding client builder

---
+++ docker-stacks/02-keycloak/etc/hooks/before-install.d/12-checkout-extension-quarkus.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -euo pipefail
+[[ "${DEBUG:-false}" == "true" ]] && set -x
+# Data deployment folder
+extension=${SIMVA_DATA_HOME}/keycloak/deployments
+
+## QUARKUS quarkus-rest-client-reactive ####
+GROUPID="io/quarkus"
+ARTIFACTID=quarkus-rest-client-reactive
+VERSION=${SIMVA_QUARKUS_VERSION:-3.9.0.CR2}
+
+URL="https://repo1.maven.org/maven2/${GROUPID}/${ARTIFACTID}/${VERSION}"
+fileName=${ARTIFACTID}-${VERSION}.jar
+jarFilePath=${extension}/${ARTIFACTID}.jar
+
+if [[ ! -e "${jarFilePath}" ]]; then 
+    curl "${URL}/${fileName}" --output "${jarFilePath}"
+fi
+
+## QUARKUS quarkus-jaxrs-client-reactive ####
+GROUPID="io/quarkus"
+ARTIFACTID=quarkus-jaxrs-client-reactive
+
+URL="https://repo1.maven.org/maven2/${GROUPID}/${ARTIFACTID}/${VERSION}"
+fileName=${ARTIFACTID}-${VERSION}.jar
+jarFilePath=${extension}/${ARTIFACTID}.jar
+
+if [[ ! -e "${jarFilePath}" ]]; then 
+    curl "${URL}/${fileName}" --output "${jarFilePath}"
+fi
+
+## QUARKUS quarkus-rest-client-reactive ####
+GROUPID="io/quarkus"
+ARTIFACTID=quarkus-resteasy-reactive-jackson
+
+URL="https://repo1.maven.org/maven2/${GROUPID}/${ARTIFACTID}/${VERSION}"
+fileName=${ARTIFACTID}-${VERSION}.jar
+jarFilePath=${extension}/${ARTIFACTID}.jar
+
+if [[ ! -e "${jarFilePath}" ]]; then 
+    curl "${URL}/${fileName}" --output "${jarFilePath}"
+fi
+## jakarta ###
+GROUPID="jakarta/ws/rs"
+ARTIFACTID=jakarta.ws.rs-api
+VERSION="3.1.0"
+
+URL="https://repo1.maven.org/maven2/${GROUPID}/${ARTIFACTID}/${VERSION}"
+fileName=${ARTIFACTID}-${VERSION}.jar
+jarFilePath=${extension}/${ARTIFACTID}.jar
+
+if [[ ! -e "${jarFilePath}" ]]; then 
+    curl "${URL}/${fileName}" --output "${jarFilePath}"
+fi
+
+## resteasy client ###
+GROUPID="org/jboss/resteasy"
+ARTIFACTID=resteasy-client
+VERSION="6.2.7.Final"
+
+URL="https://repo1.maven.org/maven2/${GROUPID}/${ARTIFACTID}/${VERSION}"
+fileName=${ARTIFACTID}-${VERSION}.jar
+jarFilePath=${extension}/${ARTIFACTID}.jar
+
+if [[ ! -e "${jarFilePath}" ]]; then 
+    curl "${URL}/${fileName}" --output "${jarFilePath}"
+fi
