893c6bfa36cbd253fe141e8f4a4b73bf1a2c3fd0
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-17 14:44:07 +0200

    fix stop when keycloak container is not running

--- docker-stacks/02-keycloak/etc/hooks/before-stop.d/10-ExportUsers.sh
+++ docker-stacks/02-keycloak/etc/hooks/before-stop.d/10-ExportUsers.sh
@@ -1,3 +1,12 @@
 if [[ $(echo ${SIMVA_KEYCLOAK_VERSION%%.*}) > 18 ]]; then 
-    docker exec -it 02-keycloak-keycloak-1 /opt/keycloak/bin/kc.sh export --dir "/opt/keycloak/data/export/" --users same_file --realm ${SIMVA_SSO_REALM:-simva} --optimized
+    # Define the name of the Docker container you want to check
+    container_name="02-keycloak-keycloak-1"
+
+    # Check if the container is running
+    if docker ps --format '{{.Names}}' | grep -q "^$container_name$"; then
+        echo "Container '$container_name' is running. Launching export of users..."
+        docker exec -it $container_name /opt/keycloak/bin/kc.sh export --dir "/opt/keycloak/data/export/" --users same_file --realm ${SIMVA_SSO_REALM:-simva} --optimized
+    else
+        echo "Container '$container_name' is not running."
+    fi
 fi;
