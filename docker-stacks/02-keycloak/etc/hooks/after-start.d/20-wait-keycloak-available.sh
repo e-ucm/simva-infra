22bc60307aec5fcd3b9a4bfe40fe79bb33c8f9f1
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-19 13:17:32 +0200

    adding wait availability

---
+++ docker-stacks/02-keycloak/etc/hooks/after-start.d/20-wait-keycloak-available.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+[[ "${DEBUG:-false}" == "true" ]] && set -x
+
+mc_max_retries=${SIMVA_MAX_RETRIES:-20}
+wait_time=${SIMVA_WAIT_TIME:-10};
+count=${mc_max_retries};
+done="ko";
+while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
+  echo 1>&2 "Checking Keycloak availability: $((${mc_max_retries}-$count+1)) pass";
+  set +e
+  if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} > 18 ]]; then 
+    wget "https://${SIMVA_SSO_HOST_SUBDOMAIN:-sso}.${SIMVA_EXTERNAL_DOMAIN:-external.test}/realms/${SIMVA_SSO_REALM:-simva}/.well-known/openid-configuration" -O - >/dev/null;
+  else 
+    wget "https://${SIMVA_SSO_HOST_SUBDOMAIN:-sso}.${SIMVA_EXTERNAL_DOMAIN:-external.test}/auth/realms/${SIMVA_SSO_REALM:-simva}/.well-known/openid-configuration" -O - >/dev/null;
+  fi
+  ret=$?;
+  set -e
+  if [ $ret -eq 0 ]; then
+    done="ok";
+  else
+    echo 1>&2 "Keycloak not available, waiting ${wait_time}s";
+    sleep ${wait_time};
+  fi;
+  count=$((count-1));
+done;
+if [ $count -eq 0 ] && [ "$done" != "ok" ]; then
+  echo 1>&2 "Keycloak not available !";
+  exit 1
+fi;
+if [ "$done" == "ok" ]; then
+  echo 1>&2 "Keycloak available !";
+fi;
