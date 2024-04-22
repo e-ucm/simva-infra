d72b494789427c4ad4e1ad2884d4d1f4d665af34
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-22 15:47:41 +0200

    fix export users

--- docker-stacks/02-keycloak/etc/hooks/before-stop.d/10-ExportUsers.sh
+++ docker-stacks/02-keycloak/etc/hooks/before-stop.d/10-ExportUsers.sh
@@ -4,7 +4,7 @@ container_name="02-keycloak-keycloak-1"
 if docker ps --format '{{.Names}}' | grep -q "^$container_name$"; then
     echo "Container '$container_name' is running. Launching export of users..."
     if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} > 18 ]]; then 
-        rm -rf "${SIMVA_CONFIG_HOME:-/home/vagrant/docker-stacks/config}/keycloak/simva-realm-export/*"
+        rm -rf ${SIMVA_CONFIG_HOME:-/home/vagrant/docker-stacks/config}/keycloak/simva-realm-export/*
         docker exec $container_name /opt/keycloak/bin/kc.sh export --dir "/opt/keycloak/data/export/" --users different_files --users-per-file 100 --realm ${SIMVA_SSO_REALM:-simva} --optimized
     else 
         #if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} > 13 ]]; then
