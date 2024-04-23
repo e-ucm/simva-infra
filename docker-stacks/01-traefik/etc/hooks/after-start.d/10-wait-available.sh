8f6d9c96c12cbd8d8e36b68301579591df2fcc31
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-23 13:32:23 +0200

    check availability of each stack

---
+++ docker-stacks/01-traefik/etc/hooks/after-start.d/10-wait-available.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+[[ "${DEBUG:-false}" == "true" ]] && set -x
+
+mc_max_retries=${SIMVA_MAX_RETRIES:-20}
+wait_time=${SIMVA_WAIT_TIME:-10};
+count=${mc_max_retries};
+done="ko";
+while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
+  echo 1>&2 "Checking Traefik availability: $((${mc_max_retries}-$count+1)) pass";
+  set +e
+  wget "https://${SIMVA_TRAEFIK_HOST_SUBDOMAIN:-traefik}.${SIMVA_EXTERNAL_DOMAIN:-external.test}/" -O - >/dev/null;
+  ret=$?;
+  set -e
+  if [ $ret -eq 0 ]; then
+    done="ok";
+  else
+    echo 1>&2 "Traefik not available, waiting ${wait_time}s";
+    sleep ${wait_time};
+  fi;
+  count=$((count-1));
+done;
+if [ $count -eq 0 ] && [ "$done" != "ok" ]; then
+  echo 1>&2 "Traefik not available !";
+  exit 1
+fi;
+if [ "$done" == "ok" ]; then
+  echo 1>&2 "Traefik available !";
+fi;
---
+++ docker-stacks/09-portainer/etc/hooks/after-start.d/10-wait-available.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+[[ "${DEBUG:-false}" == "true" ]] && set -x
+
+mc_max_retries=${SIMVA_MAX_RETRIES:-20}
+wait_time=${SIMVA_WAIT_TIME:-10};
+count=${mc_max_retries};
+done="ko";
+while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
+  echo 1>&2 "Checking Portainer availability: $((${mc_max_retries}-$count+1)) pass";
+  set +e
+  wget "https://${SIMVA_PORTAINER_HOST_SUBDOMAIN:-portainer}.${SIMVA_EXTERNAL_DOMAIN:-external.test}/" -O - >/dev/null;
+  ret=$?;
+  set -e
+  if [ $ret -eq 0 ]; then
+    done="ok";
+  else
+    echo 1>&2 "Portainer not available, waiting ${wait_time}s";
+    sleep ${wait_time};
+  fi;
+  count=$((count-1));
+done;
+if [ $count -eq 0 ] && [ "$done" != "ok" ]; then
+  echo 1>&2 "Portainer not available !";
+  exit 1
+fi;
+if [ "$done" == "ok" ]; then
+  echo 1>&2 "Portainer available !";
+fi;
