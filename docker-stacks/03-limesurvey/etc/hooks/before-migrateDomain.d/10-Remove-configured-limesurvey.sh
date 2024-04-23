9d249724805741103c8eb0d269ed7fa7e11221d8
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-23 12:41:15 +0200

    adding check of domain

--- docker-stacks/03-limesurvey/etc/hooks/before-migrateDomain.d/10-Remove-configured-limesurvey.sh
+++ docker-stacks/03-limesurvey/etc/hooks/before-migrateDomain.d/10-Remove-configured-limesurvey.sh
@@ -1,1 +1,5 @@
 rm -f "${SIMVA_CONFIG_HOME}/limesurvey/etc/config.php"
+
+# Removing Limesurvey simplesamlphp config data 
+cd "${SIMVA_DATA_HOME}/limesurvey${SIMVA_LIMESURVEY_SIMPLESAMLPHP_PATH:-/simplesamlphp}-data/config"
+rm -rf ./*
