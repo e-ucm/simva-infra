1bfaaafe5597d3b7a80a76b9ec9f25dcc21d0db0
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-23 15:38:42 +0200

    fix typo

--- docker-stacks/06-simva/etc/hooks/after-start.d/10-wait-SIMVA-Front-available.sh
+++ docker-stacks/06-simva/etc/hooks/after-start.d/10-wait-SIMVA-Front-available.sh
@@ -7,7 +7,7 @@ wait_time=${SIMVA_WAIT_TIME:-10};
 count=${mc_max_retries};
 done="ko";
 while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
-  echo 1>&2 "Checking Simva availability: $((${mc_max_retries}-$count+1)) pass";
+  echo 1>&2 "Checking Simva Front availability: $((${mc_max_retries}-$count+1)) pass";
   set +e
   wget "https://${SIMVA_EXTERNAL_DOMAIN:-external.test}/" -O - >/dev/null;
   ret=$?;
@@ -15,15 +15,15 @@ while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
   if [ $ret -eq 0 ]; then
     done="ok";
   else
-    echo 1>&2 "Simva not available, waiting ${wait_time}s";
+    echo 1>&2 "Simva Front not available, waiting ${wait_time}s";
     sleep ${wait_time};
   fi;
   count=$((count-1));
 done;
 if [ $count -eq 0 ] && [ "$done" != "ok" ]; then
-  echo 1>&2 "Simva not available !";
+  echo 1>&2 "Simva Front not available !";
   exit 1
 fi;
 if [ "$done" == "ok" ]; then
-  echo 1>&2 "Simva available !";
+  echo 1>&2 "Simva Front available !";
 fi;
--- docker-stacks/06-simva/etc/hooks/after-start.d/20-wait-SIMVA-API-available.sh
+++ docker-stacks/06-simva/etc/hooks/after-start.d/20-wait-SIMVA-API-available.sh
@@ -9,7 +9,7 @@ done="ko";
 while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
   res="{}"
   msg=""
-  echo 1>&2 "Checking Simva availability: $((${mc_max_retries}-$count+1)) pass";
+  echo 1>&2 "Checking Simva API availability: $((${mc_max_retries}-$count+1)) pass";
   set +e
   res=$(curl -s -X POST -H "Content-Type: application/json" "https://${SIMVA_SIMVA_API_HOST_SUBDOMAIN:-simva-api}.${SIMVA_EXTERNAL_DOMAIN:-external.test}/");
   msg=$(echo $res | jq ".message")
@@ -23,7 +23,7 @@ while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
   count=$((count-1));
 done;
 if [ $count -eq 0 ] && [ "$done" != "ok" ]; then
-  echo 1>&2 "Simva API  not available !";
+  echo 1>&2 "Simva API not available !";
   exit 1
 fi;
 if [ "$done" == "ok" ]; then
