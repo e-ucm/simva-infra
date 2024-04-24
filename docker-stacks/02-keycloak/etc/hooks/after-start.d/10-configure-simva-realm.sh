8b46c2b21676289c0898a0395b7bd4dadf35b659
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-04-24 12:10:25 +0200

    use docker compose exec

--- docker-stacks/02-keycloak/etc/hooks/after-start.d/10-configure-simva-realm.sh
+++ docker-stacks/02-keycloak/etc/hooks/after-start.d/10-configure-simva-realm.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} > 18 ]]; then 
     if [[ ! -e "${SIMVA_PROJECT_DIR}/.simva-initialized" ]]; then 
         echo "SIMVA is not initialized. Importing realm..." 
-        docker exec 02-keycloak-keycloak-1 /opt/keycloak/bin/kc.sh import --file "/opt/keycloak/data/import/simva-realm-full.json" --override true --optimized
+        docker compose exec keycloak /opt/keycloak/bin/kc.sh import --file "/opt/keycloak/data/import/simva-realm-full.json" --override true --optimized
     else
         echo "SIMVA is initialized." 
         migrationinProgressFile="${SIMVA_CONFIG_HOME:-/home/vagrant/docker-stacks/config}/keycloak/simva-realm-export/.migrationinprogress"
@@ -13,7 +13,7 @@ if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} > 18 ]]; then
             echo "Migration in progress. Importing realm..."
             realmFile="${SIMVA_CONFIG_HOME:-/home/vagrant/docker-stacks/config}/keycloak/simva-realm-export/${SIMVA_SSO_REALM:-simva}-realm.json" 
             if [[ -e "$realmFile" ]]; then
-                docker exec 02-keycloak-keycloak-1 /opt/keycloak/bin/kc.sh import --dir "/opt/keycloak/data/export/" --override true --optimized
+                docker compose exec keycloak /opt/keycloak/bin/kc.sh import --dir "/opt/keycloak/data/export/" --override true --optimized
             fi;
             rm -f $migrationinProgressFile
         fi;
