5ff2a19b70a657b6f39496924bd195c3b2525fa4
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-05-09 17:03:56 +0200

    fix typo

--- docker-stacks/01-traefik/etc/hooks/before-start.d/10-configure-traefik.sh
+++ docker-stacks/01-traefik/etc/hooks/before-start.d/10-configure-traefik.sh
@@ -6,4 +6,4 @@ gomplate \
     -f "${SIMVA_CONFIG_HOME}/traefik/traefik/static-conf-template/traefik.toml" \
     -o "${SIMVA_CONFIG_HOME}/traefik/traefik/static-conf/traefik.toml"
  
- chmod -R 777 "${SIMVA_CONFIG_HOME}/traefik/traefik/static-conf/"
+chmod -R 777 "${SIMVA_CONFIG_HOME}/traefik/traefik/static-conf/"
