bfde4ac004390b84a825dff0a9f5cb8eb4c8022a
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-05-21 12:17:08 +0200

    fix check keycloak available

--- docker-stacks/02-keycloak/etc/hooks/after-start.d/00-wait-keycloak-available.sh
+++ docker-stacks/02-keycloak/etc/hooks/after-start.d/00-wait-keycloak-available.sh
@@ -9,7 +9,7 @@ done="ko";
 while [ $count -gt 0 ] && [ "$done" != "ok" ]; do
   echo 1>&2 "Checking Keycloak availability: $((${mc_max_retries}-$count+1)) pass";
   set +e
-  curl "https://${SIMVA_SSO_HOST_SUBDOMAIN:-sso}.${SIMVA_EXTERNAL_DOMAIN:-external.test}";
+  curl "https://${SIMVA_SSO_HOST_SUBDOMAIN:-sso}.${SIMVA_EXTERNAL_DOMAIN:-external.test}">/dev/null;
   ret=$?;
   set -e
   if [ $ret -eq 0 ]; then
