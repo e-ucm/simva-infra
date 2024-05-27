6fe5f9b20ce08357dd6e44634953eccdcf3e7061
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-05-27 09:21:04 +0200

    adding minio docker compose + change versions.txt file to integrate various update to do at same time in the same stack

--- docker-stacks/04-minio/etc/simva.d/simva-env.sh
+++ docker-stacks/04-minio/etc/simva.d/simva-env.sh
@@ -2,4 +2,33 @@
 set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
-export COMPOSE_FILE="docker-compose.yml"
+# Extract the date part of the version string
+VERSION_DATE=$(echo $SIMVA_MINIO_VERSION | cut -d'.' -f2)
+# Convert the version date to seconds since the epoch using a specific format
+VERSION_EPOCH=$((${VERSION_DATE%%-*}))
+
+# Check if date conversion was successful
+if [ -z "$VERSION_EPOCH" ]; then
+  exit 1
+fi
+
+# Define the cutoff date (2022-01-01T00-00-00Z) and remove 'T' and 'Z'
+CUTOFF_DATE="2022-01-01T00:00:00Z"
+# Convert the cutoff date to seconds since the epoch
+CUTOFF_EPOCH=$((${CUTOFF_DATE%%-*}))
+
+# Check if date conversion was successful
+if [ -z "$CUTOFF_EPOCH" ]; then
+  exit 1
+fi
+
+# Compare the dates
+if [ $VERSION_EPOCH -ge $CUTOFF_EPOCH ]; then
+  if [[ -e "${SIMVA_DATA_HOME}/minio/migration-in-progress-fs-to-xl" ]]; then
+    export COMPOSE_FILE="minio-migration-fs-to-xl.yml"
+  else 
+    export COMPOSE_FILE="docker-compose-minio-new.yml"
+  fi
+else
+    export COMPOSE_FILE="docker-compose-minio-old.yml"
+fi
--- docker-stacks/05-kafka/etc/hooks/after-start.d/10-configure-sink.sh
+++ docker-stacks/05-kafka/etc/hooks/after-start.d/10-configure-sink.sh
@@ -60,8 +60,8 @@ if [[ ! -e "${SIMVA_CONFIG_HOME}/kafka/connect/simva-sink.json" ]]; then
 JQ_SCRIPT
 )
 
-  cat ${SIMVA_CONFIG_HOME}/kafka/connect-template/simva-sink.json | jq \
-  --arg minioUrl "https://${SIMVA_MINIO_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}" \
+  cat ${SIMVA_CONFIG_HOME}/kafka/connect/simva-sink-template.json | jq \
+  --arg minioUrl "https://${SIMVA_MINIO_HOST_SUBDOMAIN:-minio}-api.${SIMVA_EXTERNAL_DOMAIN:-external.test}/" \
   --arg minioUser "${SIMVA_KAFKA_CONNECT_SINK_USER}" \
   --arg minioSecret "${SIMVA_KAFKA_CONNECT_SINK_SECRET}" \
   --arg bucketName "${SIMVA_TRACES_BUCKET_NAME}" \
