9d2a6bbb9d1729c3c73885c6c3cfdd97a47c675f
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-05-29 08:46:18 +0200

    fix HASHED PASSWORD using env variable file in 01 traefik stack

--- docker-stacks/01-traefik/etc/hooks/before-install.d/30-generate-hashed-password-file.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[[ "${DEBUG:-false}" == "true" ]] && set -x
-
-passwordhashed=$(docker run --rm httpd:2.4-alpine htpasswd -nbB ${SIMVA_TRAEFIK_DASHBOARD_USER} ${SIMVA_TRAEFIK_DASHBOARD_PASSWORD} | cut -d ":" -f 2)
-echo $passwordhashed > "${SIMVA_CONFIG_HOME}/traefik/traefik-password-hashed.txt"
--- docker-stacks/01-traefik/etc/simva.d/simva-env.sh
+++ docker-stacks/01-traefik/etc/simva.d/simva-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export SIMVA_TRAEFIK_DASHBOARD_HASHED_PASSWORD=$(docker run --rm httpd:2.4-alpine htpasswd -nbB ${SIMVA_TRAEFIK_DASHBOARD_USER} ${SIMVA_TRAEFIK_DASHBOARD_PASSWORD} | cut -d ":" -f 2)
+
 export COMPOSE_FILE="docker-compose.yml"
 if [[ "${SIMVA_ENVIRONMENT}" == "production" ]]; then
     export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.prod.yml"
