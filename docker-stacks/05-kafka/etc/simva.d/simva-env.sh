466dff2561f2667b753d23d12621d4debe8bf3cb
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-06-04 10:26:08 +0200

    Use provectuslabs/kafka-ui instead

--- docker-stacks/05-kafka/etc/simva.d/simva-env.sh
+++ docker-stacks/05-kafka/etc/simva.d/simva-env.sh
@@ -2,5 +2,5 @@
 
 export COMPOSE_FILE="docker-compose.yml:20-kafka-connect.yml"
 if [[ "${SIMVA_ENVIRONMENT}" == "development" ]]; then
-    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.yml:10-schema-registry.yml:11-schema-registry-ui.yml:21-kafka-connect-ui.yml:30-kafka-rest-proxy.yml:99-dev-ui.yml"
+    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.yml:10-schema-registry.yml:30-kafka-rest-proxy.yml:99-dev-ui.yml"
 fi
