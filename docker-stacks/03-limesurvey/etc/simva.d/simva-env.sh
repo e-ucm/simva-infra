d38b158855bf0a3a6f9aa51a81a7ae4c0624af3a
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-06-07 11:57:25 +0200

    rename docker compose with better name

--- docker-stacks/03-limesurvey/etc/simva.d/simva-env.sh
+++ docker-stacks/03-limesurvey/etc/simva.d/simva-env.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 export COMPOSE_FILE="docker-compose.yml"
 if [[ ${SIMVA_LIMESURVEY_VERSION%.*} > 5 ]]; then
-    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-limesurvey-new.yml"
+    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-limesurvey-version-sup-5.yml"
 else 
-    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-limesurvey-old.yml"
+    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-limesurvey-custom-version-4.yml"
 fi
 if [[ "${SIMVA_ENVIRONMENT:-production}" == "development" ]]; then
     export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.yml"
