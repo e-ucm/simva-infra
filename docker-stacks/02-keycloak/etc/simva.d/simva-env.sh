d0e97cf04c5fe29968e7542afe82c1cf9c7915cb
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-06-07 12:00:15 +0200

    rename docker compose with a better name

--- docker-stacks/02-keycloak/etc/simva.d/simva-env.sh
+++ docker-stacks/02-keycloak/etc/simva.d/simva-env.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 export COMPOSE_FILE="docker-compose.yml"
 if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} -gt 18 ]]; then
-    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-keycloak-new.yml"
+    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-keycloak-version-sup-18.yml"
 else 
-    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-keycloak-old.yml"
+    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose-keycloak-version-bef-18.yml"
 fi
 if [[ "${SIMVA_ENVIRONMENT}" == "development" ]]; then
     export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.yml"
