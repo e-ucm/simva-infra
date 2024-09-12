d7b1807470300dc00485a52699db553486172f2e
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-09-12 12:43:59 +0200

    adding make storage folder

--- docker-stacks/06-simva/etc/hooks/before-start.d/11-storage.sh
+++ docker-stacks/06-simva/etc/hooks/before-start.d/11-storage.sh
@@ -3,3 +3,4 @@ set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
 mkdir -p ${SIMVA_DATA_HOME}/simva${SIMVA_STORAGE_LOCAL_PATH}
+mkdir -p ${SIMVA_DATA_HOME}/simva${SIMVA_FILTER_LOCAL_PATH}
