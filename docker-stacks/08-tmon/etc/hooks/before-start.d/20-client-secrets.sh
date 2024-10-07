7ccb9dd82ff6cfd260e9b91d158e9493284b0aa0
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-10-07 13:14:49 +0200

    Update 20-client-secrets.sh

--- docker-stacks/08-tmon/etc/hooks/before-start.d/20-client-secrets.sh
+++ docker-stacks/08-tmon/etc/hooks/before-start.d/20-client-secrets.sh
@@ -6,7 +6,7 @@ tmon_config_folder="${SIMVA_CONFIG_HOME}/tmon"
 tmon_folder="${SIMVA_DATA_HOME}/tmon"
 base_name="client_secrets"
 base_extension=".json"
-SSL_CERT_FILE="${SIMVA_TLS_HOME}/ca/rootCA.pem"
+SSL_CERT_FILE=$(echo "${SIMVA_TLS_HOME}/ca/rootCA.pem" | sed 's/\//\\\//g')
 cat "${tmon_config_folder}/${base_name}_example${base_extension}" \
     | sed  "s/<<SIMVA_TMON_DASHBOARD_HOST_SUBDOMAIN>>/${SIMVA_TMON_DASHBOARD_HOST_SUBDOMAIN}/g" \
     | sed  "s/<<SIMVA_EXTERNAL_DOMAIN>>/${SIMVA_EXTERNAL_DOMAIN}/g" \
