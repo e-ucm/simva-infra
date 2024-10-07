5266dc1705924de8b308c6d2485f2d6f62a06edf
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-10-07 13:09:19 +0200

    Update 20-client-secrets.sh

--- docker-stacks/08-tmon/etc/hooks/before-start.d/20-client-secrets.sh
+++ docker-stacks/08-tmon/etc/hooks/before-start.d/20-client-secrets.sh
@@ -6,6 +6,7 @@ tmon_config_folder="${SIMVA_CONFIG_HOME}/tmon"
 tmon_folder="${SIMVA_DATA_HOME}/tmon"
 base_name="client_secrets"
 base_extension=".json"
+SSL_CERT_FILE="${SIMVA_TLS_HOME}/ca/rootCA.pem"
 cat "${tmon_config_folder}/${base_name}_example${base_extension}" \
     | sed  "s/<<SIMVA_TMON_DASHBOARD_HOST_SUBDOMAIN>>/${SIMVA_TMON_DASHBOARD_HOST_SUBDOMAIN}/g" \
     | sed  "s/<<SIMVA_EXTERNAL_DOMAIN>>/${SIMVA_EXTERNAL_DOMAIN}/g" \
@@ -14,5 +15,5 @@ cat "${tmon_config_folder}/${base_name}_example${base_extension}" \
     | sed  "s/<<SIMVA_TMON_CLIENT_ID>>/${SIMVA_TMON_CLIENT_ID}/g" \
     | sed  "s/<<SIMVA_TMON_CLIENT_SECRET>>/${SIMVA_TMON_CLIENT_SECRET}/g" \
     | sed  "s/<<SIMVA_SIMVA_API_HOST_SUBDOMAIN>>/${SIMVA_SIMVA_API_HOST_SUBDOMAIN}/g" \
-    | sed  "s/<<SSL_CERT_FILE>>/${SIMVA_TLS_HOME}/ca/rootCA.pem/g" \
+    | sed  "s/<<SSL_CERT_FILE>>/${SSL_CERT_FILE}/g" \
     > "${tmon_folder}/${base_name}${base_extension}"
