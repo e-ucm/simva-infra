88686e5d4ca37173da50e3cde1ca9101348967be
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-03-05 16:18:33 +0100

    setting folder to keycloak extensions

--- docker-stacks/02-keycloak/etc/hooks/before-install.d/20-pull-keycloak-extensions.sh
+++ docker-stacks/02-keycloak/etc/hooks/before-install.d/20-pull-keycloak-extensions.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
+EXTENSIONS_DIR=${SIMVA_DATA_HOME}/keycloak/extensions
+
 DEPLOYMENT_DIR=${SIMVA_DATA_HOME}/keycloak/deployments
 GIT_RELEASE_URL="https://github.com/e-ucm/keycloak-extensions/releases/download/v${KEYCLOAK_EXTENSION_RELEASE}"
 if [[ ${SIMVA_KEYCLOAK_VERSION%%.*} > 18 ]]; then
@@ -14,27 +16,27 @@ fi
 
 for ext in $SIMVA_EXTENSIONS; do
     ext_jar="${ext}-${SIMVA_EXTENSIONS_VERSION}.jar"
-    if [[ ! -f "${ext_jar}" ]]; then
-        wget -q "${GIT_RELEASE_URL}/${ext_jar}"
+    if [[ ! -f "${EXTENSIONS_DIR}/${ext_jar}" ]]; then
+        wget -q -P $EXTENSIONS_DIR "${GIT_RELEASE_URL}/${ext_jar}"
         shasums="SHA256SUMS-${SIMVA_EXTENSIONS_VERSION}"
-        if [[ ! -f "${shasums}" ]]; then
-            wget -q -O "${shasums}" "${GIT_RELEASE_URL}/SHA256SUMS"
+        if [[ ! -f "${EXTENSIONS_DIR}/${shasums}" ]]; then
+            wget -q -P $EXTENSIONS_DIR -O "${shasums}" "${GIT_RELEASE_URL}/SHA256SUMS"
         fi
-        echo "$(cat "${shasums}"  | grep "${ext_jar}" | cut -d' ' -f1) ${ext_jar}" | sha256sum -c -w -s -
-        cp ${ext_jar} "$DEPLOYMENT_DIR/$ext.jar"
+        echo "$(cat "${EXTENSIONS_DIR}/${shasums}"  | grep "${ext_jar}" | cut -d' ' -f1) ${ext_jar}" | sha256sum -c -w -s -
+        cp "${EXTENSIONS_DIR}/${ext_jar}" "$DEPLOYMENT_DIR/$ext.jar"
     fi
 done
 
 ext="keycloak-events"
 ext_jar="${ext}-${SIMVA_KEYCLOAK_EVENT_EXTENSION_VERSION}.jar"
-if [[ ! -f "${ext_jar}" ]]; then
-    wget -q "https://github.com/e-ucm/keycloak-events/releases/download/v${SIMVA_KEYCLOAK_EVENT_EXTENSION_VERSION}/${ext_jar}"
+if [[ ! -f "${EXTENSIONS_DIR}/${ext_jar}" ]]; then
+    wget -P $EXTENSIONS_DIR -q "https://github.com/e-ucm/keycloak-events/releases/download/v${SIMVA_KEYCLOAK_EVENT_EXTENSION_VERSION}/${ext_jar}"
     shasums="SHA256SUMS-${SIMVA_KEYCLOAK_EVENT_EXTENSION_VERSION}"
-    if [[ ! -f "${shasums}" ]]; then
-        wget -q -O "${shasums}" "https://github.com/e-ucm/keycloak-events/releases/download/v${SIMVA_KEYCLOAK_EVENT_EXTENSION_VERSION}/SHA256SUMS"
+    if [[ ! -f "${EXTENSIONS_DIR}/${shasums}" ]]; then
+        wget -q -P $EXTENSIONS_DIR -O "${shasums}" "https://github.com/e-ucm/keycloak-events/releases/download/v${SIMVA_KEYCLOAK_EVENT_EXTENSION_VERSION}/SHA256SUMS"
     fi
-    echo "$(cat "${shasums}"  | grep "${ext_jar}" | cut -d' ' -f1) ${ext_jar}" | sha256sum -c -w -s -
-    cp ${ext_jar} "$DEPLOYMENT_DIR/$ext.jar"
+    echo "$(cat "${EXTENSIONS_DIR}/${shasums}"  | grep "${ext_jar}" | cut -d' ' -f1) ${ext_jar}" | sha256sum -c -w -s -
+    cp "${EXTENSIONS_DIR}/${ext_jar}" "$DEPLOYMENT_DIR/$ext.jar"
 fi
 
 chmod -R 777 $DEPLOYMENT_DIR
