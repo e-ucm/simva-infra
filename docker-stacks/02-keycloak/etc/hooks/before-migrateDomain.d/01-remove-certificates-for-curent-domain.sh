3cea1c5e96cc6238ec78d5260ffd5d11d82e3936
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-03-12 14:51:19 +0100

    delete file

--- docker-stacks/02-keycloak/etc/hooks/before-migrateDomain.d/01-remove-certificates-for-curent-domain.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[[ "${DEBUG:-false}" == "true" ]] && set -x
-
-${SIMVA_HOME}/bin/purge-file-if-exist.sh \
-    "${SIMVA_TLS_HOME}/${SIMVA_LIMESURVEY_SIMPLESAMLPHP_SP_CERT}" \
-    "${STACK_HOME}/extensions/simva-lti/src/main/java/es/eucm/keycloak/lti13/LTI13OIDCProtocolMapper.java"
