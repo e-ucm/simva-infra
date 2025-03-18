d93efe0ca5bc8c330ad7abd0d04869950a799d4d
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-03-18 09:58:59 +0100

    fix check docker running script

--- docker-stacks/bin/check-docker-running.sh
+++ docker-stacks/bin/check-docker-running.sh
@@ -6,10 +6,10 @@ run_in_container=${RUN_IN_CONTAINER}
 container_name=${RUN_IN_CONTAINER_NAME}
 case $run_in_container in
     "true" | 1)
-        keycloakContainer=$(echo $(docker compose ps --format '{{.Names}}' | grep "$container_name-1"))
-        echo $keycloakContainer
+        container=$(echo $(docker compose ps --format '{{.Names}}' | grep "$container_name-1"))
+        return $container
         ;;
     *)
-        echo true 
+        return true 
         ;;
 esac
