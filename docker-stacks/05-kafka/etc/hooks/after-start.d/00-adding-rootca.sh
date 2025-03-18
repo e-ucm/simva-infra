633f2a22c69cfb50498cdae4f5cc300012b487cc
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-03-18 09:47:16 +0100

    fix import root ca as root into docker startup for kafka connect

---
+++ docker-stacks/05-kafka/etc/hooks/after-start.d/00-adding-rootca.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+[[ "${DEBUG:-false}" == "true" ]] && set -x
+
+export RUN_IN_CONTAINER=true
+export RUN_IN_CONTAINER_NAME="connect"
+export RUN_IN_AS_SPECIFIC_USER="root"
+
+"${SIMVA_HOME}/bin/run-command.sh" '/usr/share/entrypoint.d/docker-startup.sh'
