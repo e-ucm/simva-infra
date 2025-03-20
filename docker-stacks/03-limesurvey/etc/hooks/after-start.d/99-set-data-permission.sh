b1dfac8e41ab13530363ea3536f98eb7f8737fa7
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-03-20 17:20:20 +0100

    fix hooks orders + update config from webhook plugins

--- docker-stacks/03-limesurvey/etc/hooks/after-start.d/99-set-data-permission.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[[ "${DEBUG:-false}" == "true" ]] && set -x
-
-chmod -R 777 ${SIMVA_DATA_HOME}/limesurvey
--- docker-stacks/03-limesurvey/etc/hooks/before-install.d/10-install-limesurvey-webhook-plugin.sh
+++ docker-stacks/03-limesurvey/etc/hooks/before-install.d/10-install-limesurvey-webhook-plugin.sh
@@ -2,13 +2,16 @@
 set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
-# Variables
-PLUGIN_NAME="LimeSuveyStatusWebhookPlugin"
-PLUGIN_SRC="$STACK_HOME/plugins/$PLUGIN_NAME"  # Local plugin directory
-PLUGIN_DEST="$SIMVA_DATA_HOME/limesurvey/data/plugins/"  # Destination
+WEBHOOK_GIT_REPO_URL="https://github.com/e-ucm/LimeSurveyWebhook.git"
+WEBHOOK_GIT_REF="adding-survey-begining"
 
-# Step 1: Copy the plugin to the LimeSurvey container
-echo "Copying plugin to LimeSurvey container plugin folder..."
-cp -r $PLUGIN_SRC $PLUGIN_DEST
-#chown -R www-data:www-data "$PLUGIN_DEST/$PLUGIN_NAME"
-chmod -R 755 "$PLUGIN_DEST/$PLUGIN_NAME"
+# Create source folder
+if [[ ! -e "${SIMVA_DATA_HOME}/limesurvey/data/plugins/LimeSurveyWebhook" ]]; then 
+    mkdir -p ${SIMVA_DATA_HOME}/limesurvey/data/plugins/LimeSurveyWebhook    
+fi
+
+# Checkout code in temp dir
+tmp_dir=$(mktemp -d)
+git clone --depth 1 --branch ${WEBHOOK_GIT_REF} ${WEBHOOK_GIT_REPO_URL} ${tmp_dir} > /dev/null 2>&1;
+
+rsync -avh --delete --itemize-changes ${tmp_dir}/ ${SIMVA_DATA_HOME}/limesurvey/data/plugins/LimeSurveyWebhook > /dev/null 2>&1
