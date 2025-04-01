3c233598053d234b469f7d2bd00caa62869f49d4
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-04-01 08:54:52 +0200

    stop if profiling containers

--- docker-stacks/07-simva/etc/simva.d/simva-env.sh
+++ docker-stacks/07-simva/etc/simva.d/simva-env.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 
 export COMPOSE_FILE="docker-compose.yml"
 if [[ "${SIMVA_ENVIRONMENT}" == "development" ]]; then
-    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.yml"
+    export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.mongo.yml"
+    if [[ ${SIMVA_ENABLE_DEBUG_PROFILING:-false} == true ]]; then
+        export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.prof.yml"
+    else 
+        export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.yml"
+    fi
 else
     export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.prod.yml"
 fi
