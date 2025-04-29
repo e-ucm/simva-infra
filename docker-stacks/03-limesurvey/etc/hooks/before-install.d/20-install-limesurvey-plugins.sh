533ce01bf23e38a1bf21de3eec64914e799e3907
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-04-29 16:38:52 +0200

    adding SurveyGuardian

--- docker-stacks/03-limesurvey/etc/hooks/before-install.d/20-install-limesurvey-plugins.sh
+++ docker-stacks/03-limesurvey/etc/hooks/before-install.d/20-install-limesurvey-plugins.sh
@@ -8,8 +8,9 @@ if [[ ! -d "${PLUGINS_DIR}" ]]; then
 fi
 
 DEPLOYMENT_DIR="${SIMVA_DATA_HOME}/limesurvey/data/plugins"
+UPLOAD_DIR="${SIMVA_DATA_HOME}/limesurvey/data/upload/plugins"
 
-declare -A plugins=(["LimeSurveyWebhook"]=${SIMVA_LIMESURVEY_WEBHOOK_PLUGIN_VERSION} ["AuthOAuth2"]=${SIMVA_LIMESURVEY_AUTHOAUTH2_PLUGIN_VERSION} ["LimeSurveyXAPITracker"]=${SIMVA_LIMESURVEY_XAPITRACKER_PLUGIN_VERSION});
+declare -A plugins=(["LimeSurveyWebhook"]=${SIMVA_LIMESURVEY_WEBHOOK_PLUGIN_VERSION} ["AuthOAuth2"]=${SIMVA_LIMESURVEY_AUTHOAUTH2_PLUGIN_VERSION} ["LimeSurveyXAPITracker"]=${SIMVA_LIMESURVEY_XAPITRACKER_PLUGIN_VERSION} ["SurveyGuardian"]=${SIMVA_LIMESURVEY_SURVEYGUARDIAN_PLUGIN_VERSION});
 
 for key in "${!plugins[@]}"; do
     ext_name=$key
@@ -31,4 +32,18 @@ for key in "${!plugins[@]}"; do
     tmp_dir=$(mktemp -d)
     unzip "${PLUGINS_DIR}/${ext_zip}" -d $tmp_dir
     rsync -avh --delete --itemize-changes ${tmp_dir}/ "${DEPLOYMENT_DIR}/$ext_name"
+    if [[ $ext_name = "SurveyGuardian" ]]; then 
+        if [[ ! -e ${UPLOAD_DIR}/ ]]; then
+            mkdir ${UPLOAD_DIR}/
+        fi
+        if [[ ! -e "${UPLOAD_DIR}/$ext_name" ]]; then
+            mkdir "${UPLOAD_DIR}/$ext_name"
+        fi
+        if [[ ! -e "${UPLOAD_DIR}/$ext_name/assets" ]]; then
+            mkdir "${UPLOAD_DIR}/$ext_name/assets"
+        fi
+        rsync -avh --delete --itemize-changes ${tmp_dir}/assets "${UPLOAD_DIR}/$ext_name/assets"
+    fi
 done
+
+
