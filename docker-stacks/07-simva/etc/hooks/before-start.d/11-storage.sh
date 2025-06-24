c3683c796125e79dc7e0c5648c7f13115d00eaa3
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-06-24 15:37:48 +0200

    fix local simva-backup-storage folder

--- docker-stacks/07-simva/etc/hooks/before-start.d/11-storage.sh
+++ docker-stacks/07-simva/etc/hooks/before-start.d/11-storage.sh
@@ -3,3 +3,9 @@ set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
 chmod -R a+w ${SIMVA_DATA_HOME}/simva/simva-trace-allocator-data
+
+if [[ ! -e "${SIMVA_DATA_HOME}/simva/simva-backup-storage" ]]; then
+    mkdir ${SIMVA_DATA_HOME}/simva/simva-backup-storage
+fi
+
+chmod -R a+w ${SIMVA_DATA_HOME}/simva/simva-backup-storage
