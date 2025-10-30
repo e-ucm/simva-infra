52636405a9e795f2d101229df80158cb33e63fba
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-10-30 16:09:41 +0100

    fix vagrant scripts

--- vagrant/2-run-vagrant-image.ps1
+++ vagrant/2-run-vagrant-image.ps1
@@ -26,6 +26,48 @@ if ($VagrantVersion) {
     return
 }
 
+# Start Powershell as administrator
+$requiredPlugins = @(
+    @{ Name = "vagrant-vbguest"; Version = "0.32.0" },    # VB Guest Additions
+    @{ Name = "vagrant-disksize"; Version = "0.1.3" }         # Disk size
+    @{ Name = "vagrant-hostmanager"; Version = "1.8.10" }     # hosts file management
+)
+
+# Function to check if a plugin exists
+function Test-VagrantPluginInstalled($pluginName, $pluginVersion) {
+    $pluginList = $(vagrant plugin list);
+
+    foreach ($line in $pluginList) {
+        if ($line -match "^\s*$pluginName\s*\(([^,]+)") {
+            Write-Output "Installed plugin: $pluginName"
+            $installedVersion = $matches[1].Trim()
+            Write-Output "Installed version: $installedVersion"
+            if (-not $pluginVersion -or $installedVersion -eq $pluginVersion) {
+                return $true
+            }
+        }
+    }
+    return $false
+}
+
+foreach ($plugin in $requiredPlugins) {
+    if (-not (Test-VagrantPluginInstalled $plugin.Name $plugin.Version)) {
+        if ($plugin.Version) {
+            Write-Output "Installing $($plugin.Name) ($($plugin.Version))..."
+            vagrant plugin install --plugin-source https://rubygems.org $($plugin.Name) --plugin-version $($plugin.Version)
+        } else {
+            Write-Output "Installing latest $($plugin.Name)..."
+            vagrant plugin install --plugin-source https://rubygems.org $($plugin.Name)
+        }
+    } else {
+        if ($plugin.Version) {
+            Write-Output "$($plugin.Name) ($($plugin.Version)) already installed."
+        } else {
+            Write-Output "$($plugin.Name) already installed."
+        }
+    }
+}
+
 # --- Auto-detect VM name from Vagrantfile ---
 try {
     $vagrantfile = Get-Content -Path "./Vagrantfile" -Raw
--- vagrant/helpers/adapter_ip.ps1
+++ vagrant/helpers/adapter_ip.ps1
@@ -1,3 +1,5 @@
+$simvaenvFile="..\docker-stacks\etc\simva.d\simva-env.sh"
+
 # Check if adapter exists
 $found = & VBoxManage list hostonlyifs
 
@@ -12,18 +14,20 @@ $ip = (VBoxManage list hostonlyifs |
          Select-Object -First 1 |
          ForEach-Object { ($_ -replace ".*IPAddress:\s*", "") })
 
+$newIp = ($ip -replace "\.1$", ".10")
+
 # Export to environment variable for Vagrant
 [System.Environment]::SetEnvironmentVariable("VBOX_HOSTONLY_IP", $ip, "Process")
-
-Set-Content ".\hostonly_ip.txt" $ip
-
-$newIp = ($ip -replace "\.1$", ".2")
+[System.Environment]::SetEnvironmentVariable("VBOX_HOSTOS_NAME", "WINDOWS", "Process")
+[System.Environment]::SetEnvironmentVariable("VBOX_EXTERNAL_IP", $newIp, "Process")
 
 Write-Host "Updated IP: $newIp"
-
-# Save to file (for logging or debugging)
-Set-Content -Path "external_ip.txt" -Value $newIp
-
 Write-Host "Detected Host-Only IP: $ip"
 
+$content = (Get-Content $simvaenvFile)
+$content -replace '^export SIMVA_HOST_EXTERNAL_IP=".*"', "export SIMVA_HOST_EXTERNAL_IP=`"$newIp`"" | Set-Content $simvaenvFile
+
+# Save to file (for logging or debugging)
+Set-Content -Path "hostonly_ip.txt" -Value $ip
+Set-Content -Path "external_ip.txt" -Value $newIp
 Set-Content -Path "os.txt" -Value "WINDOWS"
