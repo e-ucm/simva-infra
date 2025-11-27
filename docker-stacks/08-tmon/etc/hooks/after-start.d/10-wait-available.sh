f600396f9810c917b9a9999ffd6d6a17b4a3d8d4
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-11-27 12:52:39 +0100

    fix some tmon scripts

--- docker-stacks/08-tmon/etc/hooks/after-start.d/10-wait-available.sh
+++ docker-stacks/08-tmon/etc/hooks/after-start.d/10-wait-available.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
-${SIMVA_HOME}/bin/wait-available.sh "TMon" "https://${SIMVA_TMON_DASHBOARD_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}" "false" "false";
+"${SIMVA_BIN_HOME}/wait-available.sh" "TMon" "https://${SIMVA_TMON_DASHBOARD_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}" "false" "$SIMVA_TRAEFIK_FULLCHAIN_CERT_FILE";
 
 if [[ "${SIMVA_ENVIRONMENT}" == "development" ]]; then
-  ${SIMVA_HOME}/bin/wait-available.sh "Jupyter" "https://${SIMVA_JUPYTER_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}" "false" "false";
+  "${SIMVA_BIN_HOME}/wait-available.sh" "Jupyter" "https://${SIMVA_JUPYTER_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}" "false" "$SIMVA_TRAEFIK_FULLCHAIN_CERT_FILE";
 fi;
