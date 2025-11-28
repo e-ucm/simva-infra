0c67fae7ab71fa0c3e91668f98ef2dd5ed22bcf9
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-11-28 04:41:00 +0100

    fix restart after ssl fix limesurvey

--- docker-stacks/03-limesurvey/etc/hooks/after-start.d/30-ssl-fix.sh
+++ docker-stacks/03-limesurvey/etc/hooks/after-start.d/30-ssl-fix.sh
@@ -7,7 +7,7 @@ export RUN_IN_CONTAINER=true
 export RUN_IN_AS_SPECIFIC_USER="root"
 export RUN_IN_CONTAINER_NAME="limesurvey"
 
-"${SIMVA_BIN_HOME}/bin/run-command.sh" \
+"${SIMVA_BIN_HOME}/run-command.sh" \
   bash -c "if [ ! -f /etc/ssl/certs/rootCA-simva.pem ]; then
                cp /root/.limesurvey/certs/ca/rootCA.pem /etc/ssl/certs/rootCA-simva.pem
                echo '🔁  rootCA-simva.pem copied'
@@ -20,7 +20,8 @@ if [[ ! -e "${SIMVA_DATA_HOME}/limesurvey/.initializedSSL" ]]; then
     "${SIMVA_BIN_HOME}/run-command.sh" bash -c 'echo "curl.cainfo=/etc/ssl/certs/rootCA-simva.pem" >> "/usr/local/etc/php/php.ini"'
     
     # Step 2: Optional - Restart the Docker container if necessary
-    docker compose restart $RUN_IN_CONTAINER_NAME
+    source "${SIMVA_BIN_HOME}/check-docker-running.sh"
+    _restart_docker_container
     ${SIMVA_BIN_HOME}/wait-available.sh "Limesurvey" "https://${SIMVA_LIMESURVEY_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}/admin/authentication/sa/login" "true" "$SIMVA_TRAEFIK_FULLCHAIN_CERT_FILE";
 
     touch "${SIMVA_DATA_HOME}/limesurvey/.initializedSSL"
