c8f48ea9484e823d0a2cf1cd3dc27b45b5800340
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-12-04 18:41:43 +0100

    fix simva locale in simva front

--- docker-stacks/etc/simva.d/simva-env.sh
+++ docker-stacks/etc/simva.d/simva-env.sh
@@ -168,6 +168,7 @@ export SIMVA_API_LTI_MONGO_DB="/lti_simva"
 export SIMVA_SSO_USER_CAN_SELECT_ROLE="true"
 export SIMVA_SSO_ADMINISTRATOR_CONTACT="contact@administrator.com"
 export SIMVA_LTI_ENABLED="false"
+export SIMVA_I18N_DEBUG="false"
 export SIMVA_LOCALE="en,es"
 
 ##############################
