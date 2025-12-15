1223245107fdc52d7eacb79ba9fc83aeb3cd66a4
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-12-15 13:38:27 +0100

    adding docker compose mysql for simva api

--- docker-stacks/07-simva/etc/simva.d/simva-env.sh
+++ docker-stacks/07-simva/etc/simva.d/simva-env.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
 export SIMVA_LIMESURVEY_VERSION_NUMBER=${SIMVA_LIMESURVEY_VERSION%.*}
-export COMPOSE_FILE="docker-compose.yml:docker-compose.mongo.yml"
+export COMPOSE_FILE="docker-compose.yml:docker-compose.mongo.yml:docker-compose.mysql.yml"
 if [[ "${SIMVA_ENVIRONMENT}" = "development" ]]; then
     export COMPOSE_FILE="$COMPOSE_FILE:docker-compose.dev.mongo.yml"
     if [[ ${SIMVA_ENABLE_DEBUG_PROFILING:-false} = true ]]; then
