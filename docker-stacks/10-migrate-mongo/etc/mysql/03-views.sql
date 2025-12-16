dd2cf011d69d308068fb8c365cfdf1c81527b95f
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-12-16 11:56:36 +0100

    complete simlet and simlet sessions views

--- docker-stacks/10-migrate-mongo/etc/mysql/03-views.sql
+++ docker-stacks/10-migrate-mongo/etc/mysql/03-views.sql
@@ -110,10 +110,40 @@ UNION ALL
 SELECT * FROM v_activity_to_session
 ORDER BY object_type, role_id;
 
-CREATE OR REPLACE VIEW v_simlets_activities AS
-SELECT 
-	s.simlet_id,
-	s.session_id,
-	a.activity_id
-FROM SIMLETs_sessions s
-JOIN Sessions_Activities a ON a.session_id = s.session_id
+CREATE OR REPLACE VIEW v_complete_simlets AS
+SELECT
+    sim.*,
+    shlink.short_url,
+    COUNT(DISTINCT g.group_id) as total_groups,
+    COUNT(DISTINCT ses.session_id) as total_sessions,
+    GROUP_CONCAT(tag.tag SEPARATOR ', ') as tags,
+    COUNT(DISTINCT upsim.user_id) as total_direct_supervisors,
+    COUNT(DISTINCT upses.user_id) as total_direct_coordinators,
+    COUNT(DISTINCT upa.user_id) as total_direct_owners
+FROM SIMLETs sim
+LEFT JOIN SIMLETs_shlinks shlink ON sim.simlet_id = shlink.simlet_id
+LEFT JOIN SIMLETs_groups g ON sim.simlet_id = g.simlet_id
+LEFT JOIN SIMLETs_sessions ses ON sim.simlet_id = ses.simlet_id
+LEFT JOIN SIMLETs_tags tag ON sim.simlet_id = tag.simlet_id
+LEFT JOIN v_activity_to_simlet upa ON upa.object_type = "SIMLET" AND sim.simlet_id = upa.object_id
+LEFT JOIN v_session_to_simlet upses ON upses.object_type = "SIMLET" AND sim.simlet_id = upses.object_id
+LEFT JOIN v_direct_permissions upsim ON upsim.object_type = "SIMLET" AND sim.simlet_id = upsim.object_id
+GROUP BY sim.simlet_id;
+
+CREATE OR REPLACE VIEW v_simlets_sessions AS
+SELECT
+    sim.simlet_id,
+    ses.*,
+    COUNT(DISTINCT act.activity_id) as total_activities,
+    GROUP_CONCAT(tag.tag SEPARATOR ', ') as tags,
+    COUNT(DISTINCT upsim.user_id) as total_direct_supervisors,
+    COUNT(DISTINCT upses.user_id) as total_direct_coordinators,
+    COUNT(DISTINCT upa.user_id) as total_direct_owners
+FROM SIMLETs_sessions sim
+JOIN Sessions ses ON sim.session_id = ses.session_id
+LEFT JOIN Sessions_Activities act ON sim.session_id = act.session_id
+LEFT JOIN Sessions_tags tag ON sim.session_id = tag.session_id
+LEFT JOIN v_simlet_to_session upsim ON upsim.object_type = "SESSION" AND sim.simlet_id = upsim.object_id
+LEFT JOIN v_activity_to_session upa ON upa.object_type = "SESSION" AND sim.session_id = upa.object_id
+LEFT JOIN v_direct_permissions upses ON upses.object_type = "SESSION" AND sim.session_id = upses.object_id
+GROUP BY sim.simlet_id, sim.session_id;
