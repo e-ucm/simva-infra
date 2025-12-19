c32079d5848978a458054e9b22b13d67d26d6bdb
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-12-19 10:02:29 +0100

    fix sqllite

--- docker-stacks/10-migrate_mongo/etc/simva.d/simva-env.sh
+++ docker-stacks/10-migrate_mongo/etc/simva.d/simva-env.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
 export MYSQL_ROOT_HOST=$(echo $SIMVA_NETWORK_CIDR | cut -d'/' -f1 | awk -F. '{print $1 "." $2 "." $3 ".%"}')
-export COMPOSE_FILE="docker-compose.yml"
+export COMPOSE_FILE="docker-compose.yml:docker-compose.dev.yml"
