5f4e13be8b279936dd850649cfd216d5d0416ebf
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2026-01-23 01:07:41 +0100

    Remove obsolete migration files and views for SIMLET and session management

--- docker-stacks/07-simva/etc/hooks/after-start.d/10-wait-simva-simva-api-available.sh
+++ docker-stacks/07-simva/etc/hooks/after-start.d/10-wait-simva-simva-api-available.sh
@@ -8,5 +8,4 @@ if [[ -f "$SIMVA_DATA_HOME/simva/migration_sqlite_in_progress" ]]; then
 fi
 
 # Create JSON payload
-payload="{\"username\":\"$(echo $SIMVA_API_ADMIN_USERNAME | tr '[:upper:]' '[:lower:]')\",\"password\":\"$SIMVA_API_ADMIN_PASSWORD\"}"
-${SIMVA_BIN_HOME}/wait-available-with-connection.sh "SIMVA API" "https://${SIMVA_SIMVA_API_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}/users/login" "$payload" "token" "$SIMVA_TRAEFIK_FULLCHAIN_CERT_FILE"
+${SIMVA_BIN_HOME}/wait-available.sh "SIMVA API" "https://${SIMVA_SIMVA_API_HOST_SUBDOMAIN}.${SIMVA_EXTERNAL_DOMAIN}/health" "true" "$SIMVA_TRAEFIK_FULLCHAIN_CERT_FILE"
